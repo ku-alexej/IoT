613cff37f0029d9c09f8a3a5a67fa776f2695882
ku-alexej <ku.alexej@gmail.com>
2026-07-28 23:07:05 +0200

    fix: old pods file

--- p2/scripts/pods.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-echo ">>> Applying application manifests"
-kubectl apply -f /vagrant/confs/app1.yaml
-kubectl apply -f /vagrant/confs/app2.yaml
-kubectl apply -f /vagrant/confs/app3.yaml
-kubectl apply -f /vagrant/confs/ingress.yaml
