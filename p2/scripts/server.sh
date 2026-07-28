34c28b7da30e580b5fe3867bafd96cc8c9e424c0
ku-alexej <ku.alexej@gmail.com>
2026-07-28 23:13:19 +0200

    fix: flags

--- p2/scripts/server.sh
+++ p2/scripts/server.sh
@@ -16,7 +16,7 @@ curl -sfL https://get.k3s.io | sh -s - server \
     --tls-san "${SERVER_IP}"
 
 echo ">>> Waiting for K3s server to be ready..."
-until kubectl get nodes 2>/dev/null | grep -q "Ready"; do
+until kubectl get nodes 2>/dev/null | grep -qw "Ready"; do
     echo "  ... still waiting"
     sleep 5
 done
@@ -24,7 +24,7 @@ done
 sleep 20
 
 echo ">>> Setup kubectl..."
-curl -sLO https://dl.k8s.io/release/v1.35.0/bin/linux/amd64/kubectl
+curl -sfLO https://dl.k8s.io/release/v1.35.0/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 sudo mkdir -p /home/vagrant/.kube /root/.kube
@@ -46,7 +46,6 @@ EOF
     fi
 done
 sudo chown vagrant:vagrant /home/vagrant/.bashrc
-
 
 echo ">>> Applying application manifests"
 kubectl apply -f /vagrant/confs/app1.yaml
